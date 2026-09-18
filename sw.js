2337af5aea8867f3e8c32be24b8aa42ba5c34617
heri917 <arnstorecell@gmail.com>
2026-09-18 21:45:32 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="nuel-simulator-final-v1";
+const CACHE="nuel-simulator-final-v2";
 const CORE=["./","./index.html","./manifest.json","./nuel-logo.jpeg","./alamtri.webp"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
